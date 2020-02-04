54f16e41cc1b4c30d76450ee2b3ebeea921dfba9
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-04 22:37:54 +0300

    move card attrs to Card class

--- card.rb
+++ card.rb
@@ -3,6 +3,8 @@
 class Card
   attr_reader :suit
   attr_reader :value
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'].freeze
+  SUITS = %w[heart diamond club spade].freeze
 
   def initialize(suit, value)
     @suit = suit
--- deck.rb
+++ deck.rb
@@ -2,8 +2,6 @@
 
 class Deck
   attr_reader :cards
-  CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'].freeze
-  CARD_SUITS = %w[heart diamond club spade].freeze
 
   def initialize
     @cards = self.class.cards
@@ -11,8 +9,8 @@ class Deck
 
   def self.cards
     @cards = []
-    CARD_VALUES.each do |value|
-      CARD_SUITS.each do |suit|
+    Card::VALUES.each do |value|
+      Card::SUITS.each do |suit|
         card = Card.new(suit, value)
         @cards << card
       end
